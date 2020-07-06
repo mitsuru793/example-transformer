d1d5af4ff5901b1b582611b472cbef735e2537ea
mitsuru793 <mitsuru793@gmail.com>
2020-07-07 02:58:41 +0900

    clean routing

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,49 @@
 <?php
 declare(strict_types=1);
 
-use \Php\Application\Actions;
+use Php\Application\Actions\Auth;
+use Php\Application\Actions\Debug;
+use Php\Application\Actions\Post;
+use Php\Application\Actions\Seed;
+use Php\Application\Actions\Tweet;
+use Php\Application\Actions\Twitter;
+use Php\Application\Actions\UIFacesUser;
 
 global $container;
 
 return function (League\Route\Router $router) use ($container) {
-    $router->get('/', \Php\Application\Actions\Post\ListPostsAction::class);
+    $router->get('/', Post\ListPostsAction::class);
 
-    $router->post('/login', \Php\Application\Actions\Auth\LoginAction::class);
-    $router->post('/logout', \Php\Application\Actions\Auth\LogoutAction::class);
+    $router->post('/login', Auth\LoginAction::class);
+    $router->post('/logout', Auth\LogoutAction::class);
 
     $router->group('/posts', function (\League\Route\RouteGroup $r) {
-        $r->get('/{postId}', \Php\Application\Actions\Post\ShowPostAction::class);
-        $r->get('/{postId}/edit', \Php\Application\Actions\Post\EditPostAction::class);
-        $r->put('/{postId}', \Php\Application\Actions\Post\UpdatePostAction::class);
+        $r->get('/{postId}', Post\ShowPostAction::class);
+        $r->get('/{postId}/edit', Post\EditPostAction::class);
+        $r->put('/{postId}', Post\UpdatePostAction::class);
     });
 
     $router->group('/seeds', function (\League\Route\RouteGroup $r) {
-        $r->post('/', \Php\Application\Actions\Seed\StoreSeedAction::class);
+        $r->post('/', Seed\StoreSeedAction::class);
     });
 
     $router->group('/UIFacesUsers', function (\League\Route\RouteGroup $r) {
-        $r->get('/', \Php\Application\Actions\UIFacesUser\ListUIFacesUsersAction::class);
+        $r->get('/', UIFacesUser\ListUIFacesUsersAction::class);
     });
-    $router->post('/UIFacesUsers:fetch', \Php\Application\Actions\UIFacesUser\RequestGetApiAction::class);
+    $router->post('/UIFacesUsers:fetch', UIFacesUser\RequestGetApiAction::class);
 
     $router->group('/twitter', function (\League\Route\RouteGroup $r) {
-        $r->get('/oauth1/login', \Php\Application\Actions\Twitter\LoginTwitterAction::class);
-        $r->get('/oauth_callback', \Php\Application\Actions\Twitter\CallbackAction::class);
+        $r->get('/oauth1/login', Twitter\LoginTwitterAction::class);
+        $r->get('/oauth_callback', Twitter\CallbackAction::class);
 
-        $r->get('/users/{name:\w+}/home', \Php\Application\Actions\Tweet\ListUserHome::class);
+        $r->get('/users/{name:\w+}/home', Tweet\ListUserHome::class);
     });
     $router->group('/tweets', function (\League\Route\RouteGroup $r) {
-        $r->get('/home', \Php\Application\Actions\Tweet\ListMyHomeAction::class);
+        $r->get('/home', Tweet\ListMyHomeAction::class);
     });
 
     $router->group('/debug', function (\League\Route\RouteGroup $r) use ($container) {
         $templates = $container->get(\League\Plates\Engine::class);
-        $r->get('/cors-request', new Actions\Debug\RenderStaticPageAction($templates, 'debug/cors-request'));
+        $r->get('/cors-request', new Debug\RenderStaticPageAction($templates, 'debug/cors-request'));
     });
 };
