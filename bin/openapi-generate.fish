81f9f1179f587067b35d874b1da57b2a7b4c1428
mitsuru793 <mitsuru793@gmail.com>
2020-07-07 23:41:12 +0900

    add generators of php

--- bin/openapi-generate.fish
+++ bin/openapi-generate.fish
@@ -3,6 +3,34 @@
 set root (realpath (dirname (status filename))/..)
 set dist $root/openapi/generated
 
+set types \
+  # client
+  php \
+  # server-side
+  php-laravel \
+  php-lumen \
+  php-silex \
+  php-slim4 \
+  php-symfony \
+  php-ze-ph \
+
+for type in $types
+  openapi-generator generate \
+    -i ./openapi/index.yml \
+    -o $dist/$type \
+    -g $type
+end
+
+openapi-generator generate \
+  -i ./openapi/index.yml \
+  -o $dist/javascript \
+  -g javascript 
+
+openapi-generator generate \
+  -i ./openapi/index.yml \
+  -o $dist/typescript-node \
+  -g typescript-node
+
 openapi-generator generate \
   -i ./openapi/index.yml \
   -o $dist/typescript-axios \
