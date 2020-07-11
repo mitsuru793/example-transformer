1d796d3bc13ffd503e887ef7c55f5a331076239d
mitsuru793 <mitsuru793@gmail.com>
2020-07-12 08:01:00 +0900

    use autowire of di

--- config/repositories.php
+++ config/repositories.php
@@ -3,32 +3,19 @@ declare(strict_types=1);
 
 use Php\Domain\Post\PostRepository;
 use Php\Domain\Tag\TagRepository;
+use Php\Domain\Twitter\AccessToken\AccessTokenRepository;
+use Php\Domain\UIFacesUser\UIFacesUserRepository;
 use Php\Domain\User\UserRepository;
 use Php\Infrastructure\Repositories\Domain\EasyDB\EasyDBPostRepository;
 use Php\Infrastructure\Repositories\Domain\EasyDB\EasyDBTagRepository;
 use Php\Infrastructure\Repositories\Domain\EasyDB\EasyDBTwitterAccessTokenRepository;
 use Php\Infrastructure\Repositories\Domain\EasyDB\EasyDBUserRepository;
-use Php\Infrastructure\Repositories\Domain\EasyDB\ExtendedEasyDB;
 use Php\Infrastructure\Repositories\Domain\Eloquent\EloquentUIFacesUserRepository;
-use Php\Infrastructure\Tables\PostTable;
-use Php\Infrastructure\Tables\UserTable;
 
 return function (\League\Container\Container $c) {
-    $c->add(UserRepository::class, EasyDBUserRepository::class)
-        ->addArgument(ExtendedEasyDB::class)
-        ->addArgument(UserTable::class);
-
-    $c->add(PostRepository::class, EasyDBPostRepository::class)
-        ->addArgument(ExtendedEasyDB::class)
-        ->addArgument(PostTable::class)
-        ->addArgument(UserRepository::class)
-        ->addArgument(TagRepository::class);
-
-    $c->add(TagRepository::class, EasyDBTagRepository::class)
-        ->addArgument(ExtendedEasyDB::class);
-
-    $c->add(\Php\Domain\UIFacesUser\UIFacesUserRepository::class, EloquentUIFacesUserRepository::class);
-
-    $c->add(\Php\Domain\Twitter\AccessToken\AccessTokenRepository::class, EasyDBTwitterAccessTokenRepository::class)
-        ->addArgument(ExtendedEasyDB::class);
+    $c->add(UserRepository::class, fn() => $c->get(EasyDBUserRepository::class));
+    $c->add(PostRepository::class, fn() => $c->get(EasyDBPostRepository::class));
+    $c->add(TagRepository::class, fn() => $c->get(EasyDBTagRepository::class));
+    $c->add(UIFacesUserRepository::class, fn() => $c->get(EloquentUIFacesUserRepository::class));
+    $c->add(AccessTokenRepository::class, fn() => $c->get(EasyDBTwitterAccessTokenRepository::class));
 };
