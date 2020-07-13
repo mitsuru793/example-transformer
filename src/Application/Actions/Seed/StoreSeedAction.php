388c860ced777f51116f3bd1ac2742bb04f9507c
mitsuru793 <mitsuru793@gmail.com>
2020-07-14 05:15:17 +0900

    fix StoreSeedAction for user.password

--- src/Application/Actions/Seed/StoreSeedAction.php
+++ src/Application/Actions/Seed/StoreSeedAction.php
@@ -40,7 +40,7 @@ final class StoreSeedAction extends SeedAction
 
         $users = [];
         for ($i = 0; $i < 30; $i++) {
-            $user = new User(null, $faker->name);
+            $user = new User(null, $faker->name, $faker->password);
             $users[] = $this->userRepository->create($user);
         }
 
