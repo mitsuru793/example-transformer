6d31420183d463a02ce59ea265aafd4ed6d03b29
mitsuru793 <mitsuru793@gmail.com>
2020-07-18 03:12:17 +0900

    switch database when testing

--- config/dependencies.php
+++ config/dependencies.php
@@ -3,11 +3,19 @@ declare(strict_types=1);
 
 return function (\League\Container\Container $c) {
     $c->add(\Php\Infrastructure\Repositories\Domain\EasyDB\ExtendedEasyDB::class, function () {
-        $pdo = new \PDO(
-            sprintf('mysql:host=%s;dbname=%s', getenv('DB_HOST'), getenv('DB_DATABASE')),
-            getenv('DB_USER'),
-            getenv('DB_PASS'),
-        );
+        if (strpos(getenv('APP_ENV'), 'test') !== false) {
+            $pdo = new \PDO(
+                sprintf('mysql:host=%s;dbname=%s', getenv('TEST_DB_HOST'), getenv('TEST_DB_DATABASE')),
+                getenv('TEST_DB_USER'),
+                getenv('TEST_DB_PASS'),
+            );
+        } else {
+            $pdo = new \PDO(
+                sprintf('mysql:host=%s;dbname=%s', getenv('DB_HOST'), getenv('DB_DATABASE')),
+                getenv('DB_USER'),
+                getenv('DB_PASS'),
+            );
+        }
         return new \Php\Infrastructure\Repositories\Domain\EasyDB\ExtendedEasyDB($pdo, 'mysql');
     }, true);
 
