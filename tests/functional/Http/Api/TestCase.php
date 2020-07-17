a908a9e6870b34f76b42c049f2b64826bdb59810
mitsuru793 <mitsuru793@gmail.com>
2020-07-18 03:17:49 +0900

    add functional test for http

---
+++ tests/functional/Http/Api/TestCase.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types=1);
+
+namespace FunctionalTest\Http\Api;
+
+use Helper\FixtureTrait;
+use Php\Infrastructure\Repositories\Domain\EasyDB\ExtendedEasyDB;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Zend\Diactoros\ServerRequestFactory;
+
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+    use FixtureTrait;
+
+    private \Php\Application\App $app;
+
+    private \Php\Library\Fixture\Loader $loader;
+
+    protected ExtendedEasyDB $db;
+
+    private ContainerInterface $container;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->app = $this->createApp();
+        $this->loader = new \Php\Library\Fixture\Loader(__DIR__ . '/../../../fixtures.yml');
+
+        $this->db = $this->container->get(ExtendedEasyDB::class);
+        $this->db->beginTransaction();
+    }
+
+    protected function tearDown(): void
+    {
+        if ($this->db->inTransaction()) {
+            $this->db->rollback();
+        }
+    }
+
+    public function loader(): \Php\Library\Fixture\Loader
+    {
+        return $this->loader;
+    }
+
+    protected function http(string $method, string $path, array $params = []): ResponseInterface
+    {
+        $factory = new ServerRequestFactory();
+        $request = $factory->createServerRequest($method, $path);
+        $uri = $request->getUri()->withHost(\Php\Library\Util\Host::api());
+        $request = $request->withUri($uri);
+
+        if (strtolower($method) === 'get') {
+            $request = $request->withQueryParams($params);
+        }
+
+        return $this->app->process($request);
+    }
+
+    private function createApp(): \Php\Application\App
+    {
+        $this->container = new \League\Container\Container;
+        $this->container->delegate(new \League\Container\ReflectionContainer);
+
+        $add = require_once __DIR__ . '/../../../../config/dependencies.php';
+        $add($this->container);
+
+        $add = require_once __DIR__ . '/../../../../config/repositories.php';
+        $add($this->container);
+
+        return new \Php\Application\App($this->container);
+    }
+}
---
+++ tests/functional/Http/Api/Users/GetTest.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace FunctionalTest\Http\Api\Users;
+
+use FunctionalTest\Http\Api\TestCase;
+use Php\Infrastructure\Tables\UserTable;
+use Php\Library\Fixture\AliceFixture;
+
+final class GetTest extends TestCase
+{
+    private UserTable $userTable;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->userTable = new UserTable();
+    }
+
+    public function testGetUsers()
+    {
+        $f = new AliceFixture($this->fixturesRow());
+        $this->db->insertMany($this->userTable->name(), $f->get('user{1..2}', true));
+
+        $res = $this->http('GET', '/users');
+        $body = json_decode((string)$res->getBody(), true);
+
+        $this->assertSame(200, $res->getStatusCode());
+        $this->assertCount(2, $body);
+        $this->assertArrayNotHasKey('password', $body[0]);
+        $this->assertEqualsUser($f->get('user1'), $body[0]);
+        $this->assertEqualsUser($f->get('user2'), $body[1]);
+    }
+
+    private function assertEqualsUser(array $expected, array $actual)
+    {
+        $this->assertSame($expected['id'], $actual['id']);
+        $this->assertSame($expected['name'], $actual['name']);
+    }
+}
