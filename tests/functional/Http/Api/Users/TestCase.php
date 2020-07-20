3fb5ef80f8130a3f4b094e880c2832423af5b770
mitsuru793 <mitsuru793@gmail.com>
2020-07-21 03:32:14 +0900

    update test to delete user

--- tests/functional/Http/Api/Users/TestCase.php
+++ tests/functional/Http/Api/Users/TestCase.php
@@ -3,16 +3,21 @@ declare(strict_types=1);
 
 namespace FunctionalTest\Http\Api\Users;
 
+use Php\Domain\User\UserRepository;
+use Php\Infrastructure\Repositories\Domain\EasyDB\EasyDBUserRepository;
 use Php\Infrastructure\Tables\UserTable;
 
 abstract class TestCase extends \FunctionalTest\Http\Api\TestCase
 {
     protected UserTable $userTable;
 
+    protected UserRepository $userRepo;
+
     protected function setUp(): void
     {
         parent::setUp();
         $this->userTable = new UserTable();
+        $this->userRepo = new EasyDBUserRepository($this->db, $this->userTable);
     }
 
     protected function assertEqualsUser(array $expected, array $actual)
--- tests/functional/Http/Api/Users/UserId/DeleteTest.php
+++ tests/functional/Http/Api/Users/UserId/DeleteTest.php
@@ -4,22 +4,48 @@ declare(strict_types=1);
 namespace FunctionalTest\Http\Api\Users\UserId;
 
 use FunctionalTest\Http\Api\Users\TestCase;
+use Php\Domain\Post\PostRepository;
+use Php\Infrastructure\Repositories\Domain\EasyDB\EasyDBPostRepository;
+use Php\Infrastructure\Repositories\Domain\EasyDB\EasyDBTagRepository;
+use Php\Infrastructure\Repositories\Domain\EasyDB\EasyDBUserRepository;
+use Php\Infrastructure\Tables\PostTable;
+use Php\Infrastructure\Tables\TagTable;
 use Php\Library\Fixture\AliceFixture;
 
 final class DeleteTest extends TestCase
 {
+    private PostTable $postTable;
+
+    private PostRepository $postRepo;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->postTable = new PostTable();
+        $this->userRepo = new EasyDBUserRepository($this->db, $this->userTable);
+        $tagRepo = new EasyDBTagRepository($this->db, new TagTable());
+        $this->postRepo = new EasyDBPostRepository($this->db, $this->postTable, $this->userTable, $this->userRepo, $tagRepo);
+    }
+
     public function testDelete()
     {
-        $f = new AliceFixture($this->fixturesRow());
-        $this->db->insertMany($this->userTable->name(), $f->get('user{1..2}', true));
+        $f = new AliceFixture($this->fixtures());
+        $posts = $f->get('post{1..2}', true);
+        $authors = array_map(fn ($p) => $p->author, $posts);
+        $this->userRepo->createMany($authors);
+        $this->postRepo->createMany($f->get('post{1..2}', true));
 
-        $res = $this->http('DELETE', '/users/1');
+        $authorId = $authors[0]->id;
+        $res = $this->http('DELETE', "/users/$authorId");
         $body = json_decode((string)$res->getBody(), true);
 
         $this->assertSame(204, $res->getStatusCode());
         $this->assertSame([], $body);
 
-        $row = $this->db->find($this->userTable, 1);
+        $row = $this->db->find($this->userTable, $authorId);
+        $this->assertNull($row);
+
+        $row = $this->db->find($this->postTable, 1);
         $this->assertNull($row);
     }
 }
